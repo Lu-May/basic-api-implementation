2390f1b546e9dcaba0b307eb07ba0160b90b0d61
Your Name <you@example.com>
2020-09-16 14:44:05 +0800

    [luyuehuan] fix:fix

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -2,16 +2,6 @@ package com.thoughtworks.rslist.api;
 
 import com.thoughtworks.rslist.dto.RsEvent;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-<<<<<<< HEAD
-import com.thoughtworks.rslist.dto.RsEvent;
-import org.springframework.web.bind.annotation.*;
-=======
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
->>>>>>> master
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,16 +10,6 @@ import java.util.List;
 
 @RestController
 public class RsController {
-  private List<RsEvent> rsList = initList();
-
-  private List<RsEvent> initList() {
-    List<RsEvent> list = new ArrayList<>();
-    list.add(new RsEvent("第一条事件", "无分类"));
-    list.add(new RsEvent("第二条事件", "无分类"));
-    list.add(new RsEvent("第三条事件", "无分类"));
-    return list;
-  }
-<<<<<<< HEAD
   private List<RsEvent> rsList = initList();
 
   private List<RsEvent> initList() {
@@ -70,39 +50,5 @@ public class RsController {
   @DeleteMapping("/rs/rmevent/{index}")
   public void deleteRsEvent(@PathVariable int index) {
     rsList.remove(index - 1);
-=======
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
-
-  @GetMapping("/rs/list/{index}")
-  public RsEvent getOneRsEvent(@PathVariable int index) {
-    return rsList.get(index - 1);
-  }
-
-  @GetMapping("/rs/list")
-  public List<RsEvent> getRsEventWithRange(@RequestParam (required = false) Integer start,
-                                    @RequestParam (required = false) Integer end) {
-    if (start == null || end == null)
-      return rsList;
-    return rsList.subList(start - 1, end);
-  }
-
-  @PostMapping("/rs/event")
-  public void addRsEvent(@RequestBody RsEvent rsEventString) {
-    rsList.add(rsEventString);
-  }
-
-  @PutMapping("/rs/rsevent/{index}")
-  public void modifyRsEvent(@PathVariable int index, @RequestBody RsEvent rsEventString) {
-    if (rsEventString.getEventName() == null)
-      rsEventString.setEventName(rsList.get(index - 1).getEventName());
-    if (rsEventString.getKeyword() == null)
-      rsEventString.setKeyword(rsList.get(index - 1).getKeyword());
-    Collections.replaceAll(rsList, rsList.get(index - 1), rsEventString);
-  }
-
-  @GetMapping("/rs/list")
-  public String getAllRsEvent() {
-    return rsList.toString();
->>>>>>> master
   }
 }
